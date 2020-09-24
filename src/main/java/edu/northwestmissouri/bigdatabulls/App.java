85de3c9d567788ce38de52adf1f75b1a612cb406
Anil Bomma <anil.bommaa@gmail.com>
2020-09-23 23:32:17 -0500

    added pom file, src folder, test folder

---
+++ src/main/java/edu/northwestmissouri/bigdatabulls/App.java
@@ -0,0 +1,13 @@
+package edu.northwestmissouri.bigdatabulls;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome Bigdata Bulls!!!" );
+    }
+}
